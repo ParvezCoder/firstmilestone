2c9006d2dcae532cd1e9e9401e4764135b92bf04
Parvez Coder <ParvezCoder786@gmail.com>
2024-09-07 19:43:58 +0500

    fully responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,21 +17,23 @@ const Resume = () => {
           <h2 className="text-2xl  text-blue-800 font-bold border-b-2 border-blue-500 pb-2 mb-4">
             Personal Information
           </h2>
-          <div className="flex flex-col sm:flex-row justify-between space-x-4 text-white">
+          <div className="flex flex-col sm:flex-row justify-between  text-white">
             <div>
               <p className="text-lg">Name: Parvez Ahmed</p>
               <p className="text-lg">Email: parvezcoder786@gmail.Company</p>
               <p className="text-lg">Phone: 0305-2887779</p>
+              <p className="text-lg">Nationality: Pakistani</p>
+              <p className="text-lg">Religion: Iskam</p>
               <a href='https://www.linkedin.com/in/parvez-ahmed-1604b92b5/' target='_blank'>Linkedin: <span className='text-red-500 '>ParvezCoder786@gmail.com </span></a>
             </div>
             <Image src="/pic.jpg" alt="Parvez Pic"
-             width={90}  height={50} className="rounded-xl " />
+             width={120}  height={100} className="rounded-xl object-cover" />
           </div>
         </section>
 
         {/* Education Section */}
-        <section className="mb-8 ">
-          <h2 className="text-2xl  font-bold border-b-2 border-blue-500 pb-2 text-blue-800 mb-4">Education</h2>
+        <section className="mb-4 ">
+          <h2 className="text-2xl  font-bold border-b-2 border-blue-500 pb-2 text-blue-800 ">Education</h2>
           <p className="text-lg">Bachelor of Science in Computer Science</p>
           <p className="text-white">QUEST University Nawabshah (2012-2015)</p>
         </section>
@@ -43,10 +45,10 @@ const Resume = () => {
          
           {showSkills && (
             <ul className="mt-4   flex-col space-y-2  sm:flex-row justify-between text-lg flex">
-              <li className=' text-white p-1 px-2 rounded-2xl font-semibold'> JavaScript</li>
-              <li className='bg-black text-white p-1 px-2 rounded-2xl font-semibold' > React.js</li >
-              <li className='bg-black text-white p-1 px-2 rounded-2xl font-semibold'> Next.js</li>
-              <li className='bg-black text-white p-1 px-2 rounded-2xl font-semibold'> TypeScript</li>
+              <li className=' text-white p-1 px-2 rounded-2xl font-semibold'> <span className='border-b-4 border-blue-500 pb-2'>JavaScript</span></li>
+              <li className='bg-black text-white p-1 px-2 rounded-2xl font-semibold' ><span className='border-b-4 border-blue-500 pb-2'>Typescript</span></li >
+              <li className='bg-black text-white p-1 px-2 rounded-2xl font-semibold'> <span className='border-b-4 border-blue-500 pb-2'>React.js</span></li>
+              <li className='bg-black text-white p-1 px-2 rounded-2xl font-semibold'> <span className='border-b-4 border-blue-500 pb-2'>Next.js</span></li>
             </ul>
           )}
            <button className="bg-blue-500 text-white px-4 py-2 mt-4 rounded-md hover:bg-blue-600 transition"
